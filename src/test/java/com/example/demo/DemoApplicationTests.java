de871ff1f732f2f3ba91f2ae46aa2187fddd64c8
M. Raudavicius <Mindaugas.Raudavicius1@ibm.com>
2017-10-30 20:59:49 +0000

    Correct test example.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.*;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,7 +13,7 @@ public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
-    //Test.assertEquals(true, true);
+    	assertTrue(true);
 	}
 
 }
