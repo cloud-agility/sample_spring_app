479309de5be8810f800c026ebad2eb5a6d7d5239
M. Raudavicius <Mindaugas.Raudavicius1@ibm.com>
2017-10-30 21:14:57 +0000

    Test for main controller.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,16 +4,21 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import static org.junit.Assert.*;
 
+import com.example.demo.controller.MainController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+	@Autowired
+	private MainController mainController;
+
 	@Test
-	public void contextLoads() {
-    	assertTrue(true);
+	public void helloMethod() {
+    	this.mainController.index();
 	}
 
 }
